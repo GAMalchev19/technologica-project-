0a45d3a9f2d2415e9e4e452d5c7280c62d9b3e87
Boris Savov <BNSavov19@codingburgas.bg>
2020-11-15 21:17:00 +0200

    Finish landing page

--- js/parallax_logic.js
+++ js/parallax_logic.js
@@ -4,8 +4,8 @@ function parralax(e){
     this.querySelectorAll('.number').forEach(Layer => {
         const speed = Layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/100
-        const y = (window.innerHeight - e.pageY*speed)/50
+        const x = (window.innerWidth - e.pageX*speed)/300
+        const y = (window.innerHeight - e.pageY*speed)/70
 
         Layer.style.transform = `translateX(${x}px)  translateY(${y}px)`
     })
