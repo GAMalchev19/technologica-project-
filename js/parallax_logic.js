36a9a388fc9bd0984daa84875cfad60df67a344f
Boris Savov <BNSavov19@codingburgas.bg>
2020-11-27 18:02:56 +0200

    Add redesigned site and initial parallax effect

--- js/parallax_logic.js
+++ js/parallax_logic.js
@@ -1,10 +1,10 @@
 document.addEventListener("mousemove", parralax);
 function parralax(e){
 
-    this.querySelectorAll('.number').forEach(Layer => {
+    this.querySelectorAll('.parallax-layer').forEach(Layer => {
         const speed = Layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/300
+        const x = (window.innerWidth - e.pageX*speed)/70
         const y = (window.innerHeight - e.pageY*speed)/70
 
         Layer.style.transform = `translateX(${x}px)  translateY(${y}px)`
