01b8f7c9af72e78539a9db02d401ee159c1bb109
Nikola Peshev <NDPeshev19@codingburgas.bg>
2020-12-01 01:08:11 +0200

    Add svg graphics and play button

---
+++ js/handle-scrolling.js
@@ -0,0 +1,24 @@
+let tl = gsap.timeline({paused: true});
+let dur = 3;
+tl.to(".game-art-container", {
+    duration: dur,
+    y: "215vh",
+    ease: "power1.inOut"
+}).to(".intro-art-container", {
+    duration: dur,
+    y: "100vh",
+    ease: "power1.inOut"
+}, 0).to(".sections-container", {
+    duration: dur,
+    y: "100vh",
+    ease: "power1.inOut"
+}, 0).to(".intro-art-container", {
+    display: "none"
+}, 1).to(".sections-container", {
+    display: "none"
+}, 1);
+
+const btn = document.querySelector("#game-trigger");
+btn.addEventListener("click", () => {
+    tl.play();
+});
